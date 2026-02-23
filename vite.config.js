7cdc046e8a279dd5a45655fd5e06e58a1d76a10e
Alexandru Pirvulescu <alex@tcnbroadcasting.com>
2026-02-23 11:06:27 +0200

    converted to electron app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import electron from 'vite-plugin-electron';
+import renderer from 'vite-plugin-electron-renderer';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        electron([
+            {
+                // Main-process entry file of the Electron App.
+                entry: 'electron/main.ts',
+            },
+            {
+                entry: 'electron/preload.ts',
+                onstart: function (options) {
+                    // Notify the Renderer-process to reload the page when the Preload-script build is complete,
+                    // instead of restarting the entire Electron App.
+                    options.reload();
+                },
+            },
+        ]),
+        renderer(),
+    ],
 });
